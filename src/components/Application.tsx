d560915e3cf4098682f5d29bdb95be873f5f3d9e
Gahebeye aboubakar <Agahebeye@gmail.com>
2022-12-20 17:50:23 +0200

    add info and confetti

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,17 +1,22 @@
+import { clsx } from "clsx";
 import React from "react";
+import Confetti from "react-confetti";
+
 import { Die } from "./Die";
 
 export function Application() {
-  const randomValue = () => Math.floor(Math.random() * 6);
-  const randomKey = () => Math.random().toString(36).substring(2, 9);
-
+  const [won, setWon] = React.useState(false);
   const [dice, setDice] = React.useState(() =>
-    Array.from({ length: 10 }, () => ({
-      key: randomKey(),
-      value: randomValue() + 1,
-      held: false,
-    }))
+    Array.from({ length: 10 }, () => generateDice())
   );
+
+  React.useEffect(() => {
+    const allHeld = dice.every((die) => die.held);
+    const allSame = dice.every((die) => die.value === dice[0].value);
+    if (allHeld && allSame) {
+      setWon(true);
+    }
+  }, [dice]);
 
   const diceElements = (
     <div className="dice">
@@ -20,7 +25,7 @@ export function Application() {
           hold={() => holdDie(die.key)}
           key={die.key}
           held={die.held}
-          value={die.value}
+          value={die.value + 1}
         />
       ))}
     </div>
@@ -28,22 +33,43 @@ export function Application() {
 
   return (
     <div className="application">
+      {won && <Confetti />}
       <div>
+        <div>
+          {won ? (
+            <p className="congratulations">Congratulations!!!</p>
+          ) : (
+            <p className="roll-info">Roll dice till they all become the same</p>
+          )}
+        </div>
         {diceElements}
-        <button onClick={resetDice} className="button-reset">
-          reset
+        <button
+          onClick={rollDice}
+          className={clsx("button", { "button-inverse": won })}
+        >
+          {won ? "play new game" : "roll"}
         </button>
       </div>
     </div>
   );
 
-  function resetDice() {
-    setDice((prevDice) => {
-      return prevDice.map((die) => ({
-        ...die,
-        value: randomValue(),
-      }));
-    });
+  function generateDice() {
+    return {
+      key: Math.random().toString(36).substring(2, 9),
+      value: Math.floor(Math.random() * 6),
+      held: false,
+    };
+  }
+
+  function rollDice() {
+    if (won) {
+      setDice(() => Array.from({ length: 10 }, () => generateDice()));
+      setWon(false);
+    } else {
+      setDice((prevDice) => {
+        return prevDice.map((die) => (die.held ? die : generateDice()));
+      });
+    }
   }
 
   function holdDie(key: string | number) {
